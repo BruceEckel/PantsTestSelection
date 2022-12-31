9b719a91f3b7ffc58bf097d2881dbbbd8bf55a0b
Bruce Eckel <BruceTEckel@gmail.com>
2022-12-31 11:23:19 -0700

    +

--- succeed/ex1/tests.py
+++ succeed/ex1/tests.py
@@ -1,7 +1,7 @@
-def test_not1():
+def test_not3():
   print("THIS IS NOT A TEST")
   assert False
 
-def test_1_check():
+def test_3_check():
   print("THIS IS A TEST IN PYTEST BUT NOT IN PANTS")
   assert True
